6b71a821757494ce5a18ec165ce2a6a9fd279a81
sforaaleksander <sforaaleksander@gmail.com>
2020-09-09 18:02:11 +0200

    implement Cart.java

---
+++ src/main/java/com/codecool/servlet/models/Cart.java
@@ -0,0 +1,24 @@
+package com.codecool.servlet.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Item> itemList;
+
+    public Cart() {
+        itemList = new ArrayList<>();
+    }
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public void addItem(Item item){
+        itemList.add(item);
+    }
+
+    public void removeItem(Item item) {
+        itemList.remove(item);
+    }
+}
